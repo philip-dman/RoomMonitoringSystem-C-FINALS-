0b1082180678cb92e199479c058bf1ab04355e01
hanzm <hanzm@HANZDESKTOP>
2024-12-11 20:17:36 +0800

    Add project files.

---
+++ RoomMonitoringSystem(C#FINALS)/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RoomMonitoringSystem_C_FINALS_
+{
+    public partial class LogInPage : Form
+    {
+        public LogInPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
